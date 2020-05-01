e4c15d8549537a6ab28a2198f4ac942433cfa027
John Cosper <Johngcosper@gmail.com>
2020-05-01 16:18:28 -0400

    Final working Application

--- generateHtml.js
+++ generateHtml.js
@@ -1,49 +1,66 @@
-function generateHtml(roleName) {
+const path = require("path");
+const fs = require("fs");
 
-    if (roleName === 'Manager') {
-        return `<div class="card employee-card">
-        <div class="card-header">
-            <h2 class="card-title">${response.name}</h2>
-            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${response.role}</h3>
-        </div>
-        <div class="card-body">
-            <ul class="list-group">
-                <li class="list-group-item">ID: ${response.id}</li>
-                <li class="list-group-item">Email: <a href="mailto:${response.email}">${response.email}</a></li>
-                <li class="list-group-item">Office number: ${response.office}</li>
-            </ul>
-        </div>
-    </div>`
-    } else if (roleName === 'Engineer') {
-        return `<div class="card employee-card">
-        <div class="card-header">
-            <h2 class="card-title">${response.name}</h2>
-            <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${response.role}</h3>
-        </div>
-        <div class="card-body">
-            <ul class="list-group">
-                <li class="list-group-item">ID: ${response.id}</li>
-                <li class="list-group-item">Email: <a href="mailto:${response.email}">${response.email}</a></li>
-                <li class="list-group-item">GitHub: <a href="https://github.com/${response.github}" target="_blank" rel="noopener noreferrer">${response.github}</a></li>
-            </ul>
-        </div>
-    </div>`
-    } else if (roleName === "Intern") {
-        return `<div class="card employee-card">
-        <div class="card-header">
-            <h2 class="card-title">${response.name}</h2>
-            <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${response.role}</h3>
-        </div>
-        <div class="card-body">
-            <ul class="list-group">
-                <li class="list-group-item">ID: ${response.id}</li>
-                <li class="list-group-item">Email: <a href="mailto:${response.email}">${response.email}</a></li>
-                <li class="list-group-item">School: ${response.school}</li>
-            </ul>
-        </div>
-    </div>`
-    }
+const templatesDirectory = path.resolve(__dirname, "./templates");
 
-}
+const generate = employees => {
+  const empArray = [];
 
-module.exports = generateHtml;
+  empArray.push(employees
+    .filter(employee => employee.getRole() === "Manager")
+    .map(manager => generateManager(manager))
+  );
+  empArray.push(employees
+    .filter(employee => employee.getRole() === "Engineer")
+    .map(engineer => generateEngineer(engineer))
+  );
+  empArray.push(employees
+    .filter(employee => employee.getRole() === "Intern")
+    .map(intern => generateIntern(intern))
+  );
+
+  return generateMain(empArray.join(""));
+
+};
+
+const generateManager = manager => {
+  let template = fs.readFileSync(path.resolve(templatesDirectory, "manager.html"), "utf8");
+  template = replacePlaceholders(template, "name", manager.getName());
+  template = replacePlaceholders(template, "role", manager.getRole());
+  template = replacePlaceholders(template, "email", manager.getEmail());
+  template = replacePlaceholders(template, "id", manager.getId());
+  template = replacePlaceholders(template, "officeNumber", manager.getOfficeNumber());
+  return template;
+};
+
+const generateEngineer = engineer => {
+  let template = fs.readFileSync(path.resolve(templatesDirectory, "engineer.html"), "utf8");
+  template = replacePlaceholders(template, "name", engineer.getName());
+  template = replacePlaceholders(template, "role", engineer.getRole());
+  template = replacePlaceholders(template, "email", engineer.getEmail());
+  template = replacePlaceholders(template, "id", engineer.getId());
+  template = replacePlaceholders(template, "github", engineer.getGithub());
+  return template;
+};
+
+const generateIntern = intern => {
+  let template = fs.readFileSync(path.resolve(templatesDirectory, "intern.html"), "utf8");
+  template = replacePlaceholders(template, "name", intern.getName());
+  template = replacePlaceholders(template, "role", intern.getRole());
+  template = replacePlaceholders(template, "email", intern.getEmail());
+  template = replacePlaceholders(template, "id", intern.getId());
+  template = replacePlaceholders(template, "school", intern.getSchool());
+  return template;
+};
+
+const generateMain = empArray => {
+  const template = fs.readFileSync(path.resolve(templatesDirectory, "main.html"), "utf8");
+  return replacePlaceholders(template, "team", empArray);
+};
+
+const replacePlaceholders = (template, placeholder, value) => {
+  const pattern = new RegExp("{{ " + placeholder + " }}", "gm");
+  return template.replace(pattern, value);
+};
+
+module.exports = generate;
--- index.js
+++ index.js
@@ -4,167 +4,114 @@ const Intern = require("./lib/intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const generateHtml = require('./generateHtml');
+const generate = require('./generateHtml');
 
-
-async function promptRole(roleName) {
-    return await inquirer.prompt([
-        {
-            type: 'list',
-            message: 'Select Role',
-            choices: [{ name: 'Manager'}, { name: 'Engineer'}, { name: 'Intern'}, { name: 'Exit'}],
-            name: 'role'
-        },
-    ]);
-}
-
-async function promptInfo(roleName) {
-    if (roleName === 'Manager') {
-        const response = await inquirer.prompt([
+let employees = [];
+async function promptUser() {
+    return inquirer
+        .prompt(
             {
-                type: 'input',
-                message: 'Name',
-                name: 'name'
+                type: 'list',
+                message: 'Select Role',
+                name: 'role',
+                choices: ["Manager", "Engineer", "Intern", "Exit"]
             },
-            {
-                type: 'input',
-                message: 'Employee ID',
-                name: 'id'
-            },
-            {
-                type: 'input',
-                message: 'Email',
-                name: 'email'
-            },
-            {
-                type: 'input',
-                message: 'Office Number',
-                name: 'office'
-            }
-        ])
-        return new Manager(response.name, response.id, response.email, response.office);
-    } else if (roleName === 'Engineer') {
-        const response = await inquirer.prompt([
-            {
-                type: 'input',
-                message: 'Name',
-                name: 'name'
-            },
-            {
-                type: 'input',
-                message: 'Employee ID',
-                name: 'id'
-            },
-            {
-                type: 'input',
-                message: 'Email',
-                name: 'email'
-            },
-            {
-                type: 'input',
-                message: 'Github Username',
-                name: 'github'
-            }
-        ]);
-        return new Engineer(response.name, response.id, response.email, response.github);
-    } else if (roleName === 'Intern') {
-        const response = await inquirer.prompt([
-            {
-                type: 'input',
-                message: 'Name',
-                name: 'name'
-            },
-            {
-                type: 'input',
-                message: 'Employee ID',
-                name: 'id'
-            },
-            {
-                type: 'input',
-                message: 'Email',
-                name: 'email'
-            },
-            {
-                type: 'input',
-                message: 'University',
-                name: 'school'
-            }
-        ]);
-        return new Intern(response.name, response.id, response.email, response.school);
-    };
-};
-
-async function a() {
-    let response1 = {role: 'Manager'};
-    let response2 = await promptInfo(response1.role);
-    let empArr = [];
-    empArr.push(response2);
-
-    console.log(response2);
-
-    while (response1.role !== "Exit") {
-        response1 = await promptRole();
-        response2 = await promptInfo(response1.role);
-
-        if (response2) {
-            empArr.push(response1, response2);
-            empArr.forEach((person) => {
-                if (person.role === 'Manager') {
-                    console.log('yay');
-                    let managerTemp = fs.readFileSync('./templates/manager.html', 'utf8');
-                    let managerReplace = managerTemp.replace('{{ name }}', person.name);
-                    managerReplace += managerTemp.replace('{{ role }}', person.role);
-                    fs.writeFileSync('test.html', managerReplace, 'utf8');
-                }
-            });
-        };
-    };
-    console.log(empArr);
-}
-
-a();
-
-// let manager = fs.readFileSync('./templates/manager', 'utf8');
-// let managerReplace = manager.replace('{{ name }}', empArr[i].name);
-// managerReplace += manager.replace('{{ role }}', ampArr[i].role);
-// managerReplace += manager.replace('{{ id }}', ampArr[i].id);
-// managerReplace += manager.replace('{{ email }}', ampArr[i].email);
-// managerReplace += manager.replace('{{ officeNumber }}', ampArr[i].office);
-// let managerResult = fs.writeFileSync('test.html', managerReplace, 'utf8');
-// console.log(managerResult);
-
-/*.then(async function() {
-
-    console.log(response1);
-    console.log(response2);
-
-    const render = generateHtml(response1, response2)
-
-    async function i() {
-        let user = fs.writeFileSync('test.html', render, function(error) {
-            if (error) {
-                return console.log(error);
-            } else {
-                console.log('success')
+        )
+        .then(({ role }) => {
+            if (role === "Manager") {
+                return inquirer.prompt([
+                    {
+                        type: 'input',
+                        message: 'Name',
+                        name: 'name'
+                    },
+                    {
+                        type: 'input',
+                        message: 'Employee ID',
+                        name: 'id'
+                    },
+                    {
+                        type: 'input',
+                        message: 'Email',
+                        name: 'email'
+                    },
+                    {
+                        type: 'input',
+                        message: 'Office Number',
+                        name: 'officeNumber'
+                    }
+                ])
+                .then(({name, id, email, officeNumber}) => {
+                    employees.push(new Manager(name, id, email, officeNumber))
+                    promptUser();
+                })
+            }else if (role === "Engineer") {
+                return inquirer.prompt([
+                    {
+                        type: 'input',
+                        message: 'Name',
+                        name: 'name'
+                    },
+                    {
+                        type: 'input',
+                        message: 'Employee ID',
+                        name: 'id'
+                    },
+                    {
+                        type: 'input',
+                        message: 'Email',
+                        name: 'email'
+                    },
+                    {
+                        type: 'input',
+                        message: 'Github Username',
+                        name: 'github'
+                    },
+                ])
+                .then(({name, id, email, github}) => {
+                    employees.push(new Engineer(name, id, email, github))
+                    promptUser();
+                })
+            }else if (role === "Intern"){
+                return inquirer.prompt([
+                    {
+                        type: 'input',
+                        message: 'Name',
+                        name: 'name'
+                    },
+                    {
+                        type: 'input',
+                        message: 'Employee ID',
+                        name: 'id'
+                    },
+                    {
+                        type: 'input',
+                        message: 'Email',
+                        name: 'email'
+                    },
+                    {
+                        type: 'input',
+                        message: 'University',
+                        name: 'school'
+                    },
+                ])
+                .then(({name, id, email, school}) => {
+                    employees.push(new Intern(name, id, email, school))
+                    promptUser();
+                })
+            }else if (role === "Exit") {
+                console.log(employees)
+                const employeeHtml = generate(employees)
+                fs.writeFile(outputPath, employeeHtml, function(err) {
+                    if (err) {
+                      throw err;
+                    }
+                    console.log("We did it boys!");
+                  });
             }
         });
-    }
-
-    i();
-
-    async function e() {
-        let team = fs.readFileSync('./templates/main.html', 'utf8');
-        let user = fs.readFileSync('./test.html', 'utf8');
-        console.log(typeof user);
-        var result = team.replace('{team}', user);
-        fs.writeFileSync('./team.html', result, 'utf8');
-    }
-
-    e();
-
-});
-*/
+};
+promptUser();
